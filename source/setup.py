247b02e3e122270aaf48c3770d943f1c14023618
Bruno Thiago Rego Valeriano Silva <brunothiagorvs@gmail.com>
2021-06-21 00:53:02 -0300

    Baixar importâncias

--- source/setup.py
+++ source/setup.py
@@ -10,3 +10,6 @@ for target, _ in targets_models.items():
 
 gdd.download_file_from_google_drive(file_id='1CLdiHuNq_QNOcv6fTs8HlOnlScE9RNCO',
                                     dest_path='sql/data.db', overwrite=True, showsize=True)
+
+gdd.download_file_from_google_drive(file_id='1lKhaK1dir8ywBHf22kbnGjTHuKsJ2-i4',
+                                    dest_path='model/importances.pkl', overwrite=True, showsize=True)
